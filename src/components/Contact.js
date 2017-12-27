0174d2bec0e3b86a5bc7014ed38875b536d3c001
Andrew Chavez <chavandrew@gmail.com>
2017-12-27 00:21:18 -0600

    added contact info/email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,9 +7,9 @@ import gmail from '../assets/img/gmail1.png'
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <div className='git-pic'><img src={git}/></div>
-        <div className='gmail1-pic'><img src={gmail}/> </div>
+      <div className='flex'>
+        <div className='git-pic'><img src={git}/><a href='https://github.com/chavandrew' target='_blank' className='git'>chavandrew</a></div>
+        <div className='gmail1-pic'><img src={gmail}/><a href='mailto:chavandrew@gmail.com' target='_blank' className='git'>chavandrew@gmail.com</a> </div>
 
      </div>
 
