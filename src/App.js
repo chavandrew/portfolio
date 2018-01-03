6312d9ba59f819d9d5c8068a68b14891697bd6cb
Andrew Chavez <chavandrew@gmail.com>
2018-01-03 11:12:14 -0600

    added footer layout/info

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
         <Router>
               <div>
                 <div className="response">
@@ -34,7 +35,7 @@ class App extends Component {
                   <NavLink className='link' to='/resume'>resume</NavLink>{' '}
                   <NavLink className='link' to='/contact'>contact</NavLink>{' '}
                 </nav>
-
+            
             </div>
 
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-
+import './Footer.css';
 
 
 class Footer extends Component {
   render() {
     return (
       <div>
-      
+      <ul>
+        <li>chavandrew@gmail.com</li>
+          <li>806 632-4182</li>
+            <li>Austin, TX</li>
+      </ul>
 
       </div>
 
