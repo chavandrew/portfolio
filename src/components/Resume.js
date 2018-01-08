13e4b6ecb3f837d1a2f8889ce6023271b7328413
Andrew Chavez <chavandrew@gmail.com>
2018-01-08 15:29:22 -0600

    link to pdf resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,7 +8,7 @@ class Resume extends Component {
       <div className='flex-b'>
         {/* <p className='click'> (click resume for pdf version)</p> */}
 
-      <div className='resumecont'><a className='link' href='https://drive.google.com/drive/my-drive' target='_blank'><img src={resume}/></a></div>
+      <div className='resumecont'><a className='link' href='https://drive.google.com/open?id=1EH58GuHit07t16dd0Nu5HFO9ubjX_t1Y' target='_blank'><img src={resume}/></a></div>
 
     </div>
   );
