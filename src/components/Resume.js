073702827342a4f28afb25c2cc30884646171a3d
Andrew Chavez <chavandrew@gmail.com>
2018-01-08 15:20:01 -0600

    added updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Resume.css'
-import resume from '../assets/img/resume-1.png'
+import resume from '../assets/img/atomresume.png'
 
 class Resume extends Component {
   render() {
@@ -8,7 +8,7 @@ class Resume extends Component {
       <div className='flex-b'>
         {/* <p className='click'> (click resume for pdf version)</p> */}
 
-      <div className='resumecont'><a className='link' href='https://drive.google.com/file/d/13ygFQoCzEg15SX2jGtCeHP4GnIH_wLfP/view?ths=true' target='_blank'><img src={resume}/></a></div>
+      <div className='resumecont'><a className='link' href='https://drive.google.com/drive/my-drive' target='_blank'><img src={resume}/></a></div>
 
     </div>
   );
