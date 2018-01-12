26a9047dbdc95081a69c36adc0231549e31a035c
Andrew Chavez <chavandrew@gmail.com>
2018-01-11 20:20:14 -0600

    portfolio updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,24 +13,24 @@ class Projects extends Component {
 
     <section className="responsive">
 
-      <ul>
+      <ul className='flexx'>
         <li> <div className='grow'><img src={eargsound}/></div>
-          <h3>Eargasmr</h3>
-          <p> ASMR is growing rapidly in popularity. Currently users almost exclusively use YouTube and ASMRtists accrue millions of views for their soothing sound effects. Our app allows users to create their own ASMR sessions with sounds of their choosing. React Native & React-Native-Sound</p>
+          <h3 className='para'>Eargasmr</h3>
+          <p className='para'> ASMR is growing rapidly in popularity. Currently users almost exclusively use YouTube and ASMRtists accrue millions of views for their soothing sound effects. Our app allows users to create their own ASMR sessions with sounds of their choosing. React Native & React-Native-Sound</p>
         </li>
         <li><div className='grow'><img src={flippers}/></div>
 
-          <h3>Flippers</h3>
-          <p>A website for a pinball tavern in Lubbock, TX currently in developement. Basic outline of the website-- will add CMS so the tavern can update content as needed. React  </p><a href="https://flippers-tavern.firebaseapp.com/" target='_blank'>View more.</a>
+          <h3 className='para'>Flippers</h3>
+          <p className='para'>A website for a pinball tavern in Lubbock, TX currently in developement. Basic outline of the website-- will add CMS so the tavern can update content as needed. React  </p><a href="https://flippers-tavern.firebaseapp.com/" target='_blank'>View more.</a>
         </li>
         <li><div className='grow'><img src={pupfetch}/></div>
-          <h3>Pupfetchr</h3>
-          <p>PupFetchr is an app designed to help people search for a dog from local dog shelters in Austin, TX for their next furry friend.  Using the Petfinder API a list of dogs is returned. Worked on the back-end of this group project. jQuery, Mongoose, MEN stack, Express.</p><a href="http://pupfetchrs.herokuapp.com/" target='_blank'>View more.</a>
+          <h3 className='para'>Pupfetchr</h3>
+          <p className='para'>PupFetchr is an app designed to help people search for a dog from local dog shelters in Austin, TX for their next furry friend.  Using the Petfinder API a list of dogs is returned. Worked on the back-end of this group project. jQuery, Mongoose, MEN stack, Express.</p><a href="http://pupfetchrs.herokuapp.com/" target='_blank'>View more.</a>
         </li>
         <li><div className='grow'><img src={tictac}/></div>
 
-          <h3>Tic-Tac-Toe</h3>
-          <p> The classic but always fun game of tic-tac-toe. jQuery, Bootstrap, HTML, CSS</p><a href="#">View more.</a>
+          <h3 className='para'>Tic-Tac-Toe</h3>
+          <p className='para'> The classic but always fun game of tic-tac-toe. jQuery, Bootstrap, HTML, CSS</p><a href="#">View more.</a>
         </li>
       </ul>
     </section>
