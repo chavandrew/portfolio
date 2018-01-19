720a50379cab90c938b84bd17dadcc561aff4ef1
Andrew Chavez <chavandrew@gmail.com>
2018-01-19 12:28:38 -0600

    landing page bug fix

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
                     <p className='dev'> web developer</p>
 
                 <nav className='tabs'>
-                  <NavLink className='link' exact to="/portfolio">home</NavLink>{' '}
+                  <NavLink className='link' exact to="/portfolio/">home</NavLink>{' '}
                   <NavLink className='link' to='/projects'>projects</NavLink>{' '}
                   <NavLink className='link' to='/resume'>resume</NavLink>{' '}
                   <NavLink className='link' to='/contact'>contact</NavLink>{' '}
@@ -41,7 +41,7 @@ class App extends Component {
 
 
                 <Switch>
-                  <Route exact path="/portfolio" component={Home} />
+                  <Route exact path="/portfolio/" component={Home} />
                  <Route path="/projects" component={Projects} />
                  <Route path="/resume" component={Resume} />
                  <Route path="/contact" component={Contact} />
